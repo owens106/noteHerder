fd775dd0502880d1aa33205138b2c26376114403
Colin <owens106@purde.edu>
2018-05-22 14:45:24 -0400

    updated style for main

--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,30 @@
 import React from 'react'
+
 import Sidebar from './Sidebar'
 import NoteList from './NoteList'
 import NoteForm from './NoteForm'
 
+
+
 const Main = () =>{
     return(
-        <div className="Main">
+        <div 
+          className="Main"
+          style={style}
+
+        >
           <Sidebar />
           <NoteList />
           <NoteForm />
         </div>
     )
+}
+
+const style ={
+    display: 'flex',
+    height: '100vh',
+    alignItems: 'stretch',
+
 }
 
 export default Main
