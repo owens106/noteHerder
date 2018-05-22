10a24e4ab00ce2aa5c3eca63f5f8fc56a26d2c77
Colin <owens106@purde.edu>
2018-05-22 14:31:41 -0400

    add sidebar notelist and noteform components

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import './App.css';
 import Main from './Main'
 
 
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Main />
+    
       </div>
     );
   }
