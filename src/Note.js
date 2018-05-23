0801f6bbb3248b50435df55743207e2b38e211a2
Colin <owens106@purde.edu>
2018-05-23 14:35:27 -0400

    dynamically enter notes from array

--- src/Note.js
+++ src/Note.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const Note =() =>{
+const Note =(props) =>{
     return(
     <li className='Note'>
             <div className="note">
               <div className="note-title">
-                Kohlrabi welsh
+                {props.note.title}
               </div>
               <div className="note-body">
                 <p>
-                  Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.
+                    {props.note.body}
                 </p>
               </div>
             </div>
--- src/NoteList.js
+++ src/NoteList.js
@@ -4,12 +4,29 @@ import './NoteList.css'
 import Note from './Note'
 
 const NoteList = () => {
-  const notes=[1,2,3,4,5,6]
+  const notes=[
+    {
+      id: 1,
+      title: 'js Thoughs',
+      body: 'cool bean',
+    },
+    {
+      id: 2,
+      title: 'Breakfast',
+      body: 'FOR',
+    },
+    {
+      id: 3,
+      title: 'gg no re',
+      body: 'radical dude',
+    }
+
+  ]
   return (
     <div className="NoteList">
       <h3>Notes</h3>
       <ul id="notes">
-        {notes.map(note => <Note />)}  
+        {notes.map(note => <Note note={note} />)}  
       
       </ul>
     </div>
