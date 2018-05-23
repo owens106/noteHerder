2d3d7053b2957b0be34a6358b31ecbe751941705
Colin <owens106@purde.edu>
2018-05-23 14:13:14 -0400

    updated to fontawesome 5 with svg's

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,123 +1,99 @@
 import React from 'react'
+import { StyleSheet, css } from 'aphrodite'
 
 import quill from './quill.svg'
-import newHover from './new-hover.png'
 import newIcon from './new.png'
+import newHover from './new-hover.png'
 
-
-
-const Sidebar = () =>{
-  
-    
-    return(
-        
-        <nav 
-            className="Sidebar"
-            style={styles.sidebar}
-        >
-          
-
-          <div 
-            className="logo"
-            style={styles.logo}
-        >
-            <img 
-                src={quill} 
-                alt="Noteherder"
-                style={styles.logoImg}
-            />
-          </div>
-          <a className="new-note" href="/notes">
-            <img 
-                src={newHover} 
-                alt="New note" 
-                style={styles.sidebarButton}
-                onMouseEnter={function (ev){
-                    ev.target.setAttribute('src', newIcon)
-
-                }}
-                onMouseLeave={function (ev){
-                    ev.target.setAttribute('src', newHover)
-
-                }}
-            />
-            <img 
-                className="outline" 
-                src={newIcon} 
-                alt="New note"
-                style={styles.sidebarNewNote}
-            />
-          </a>
-          <div 
-            className="SignOut"
-            style={styles.sidebarSignout}
-          >
-            <button>
-              <i 
-                className="fa fa-sign-out"
-                style={styles.sidebarSignoutButtonFA}
-              >
-              </i>
-            </button>
-          </div>
-        </nav>
-
-    )
+const Sidebar = () => {
+  return (
+    <nav className={css(styles.sidebar)}>
+      <div
+        className={css(styles.logo)}
+      >
+        <img
+          src={quill}
+          alt="Noteherder"
+          className={css(styles.logoImg)}
+        />
+      </div>
+      <a
+        href="/notes"
+        className={css(styles.newNote)}
+      >
+        <img
+          src={newHover}
+          alt="New note"
+          className={css(styles.newNoteImg)}
+        />
+        <img
+          src={newIcon}
+          alt="New note"
+          className={css(styles.newNoteImg, styles.newNoteImgHover)}
+        />
+      </a>
+      <div
+        className={css(styles.signOut)}
+      >
+        <button className={css(styles.button)}>
+          <i
+            className={`fas fa-sign-out-alt ${css(styles.buttonIcon)}`}
+            title='signout'
+          ></i>
+        </button>
+      </div>
+    </nav>
+  )
 }
 
-
-const styles={
-    sidebar:{
-        width: '8rem',
-        backgroundColor: '#f3f3f3',
-        padding: '0.5rem 0',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
+const styles = StyleSheet.create({
+  sidebar: {
+    width: '6rem',
+    backgroundColor: '#f3f3f3',
+    padding: '0.5rem 0',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  logo: {
+    fontFamily: '"Fauna One"',
+    color: '#666',
+    fontSize: '3rem',
+  },
+  logoImg: {
+    width: '3rem',
+    paddingLeft: '0.4rem',
+  },
+  newNote: {
+    marginTop: '2rem',
+    position: 'relative',
+    width: '4rem',
+  },
+  newNoteImg: {
+    position: 'absolute',
+    left: '0',
+    width: '100%',
+    transition: 'opacity 0.25s ease-in-out',
+  },
+  newNoteImgHover: {
+    ':hover': {
+      opacity: 0,
     },
-    logo:{
-        fontFamily: '"Fauna One"',
-        color: '#666',
-        fontSize: '3rem',
-
-    },
-    logoImg: {
-        width: '3rem',
-        paddingLeft: '0.4rem',
-        
-    },
-    sidebarNewNote: {
-        marginTop: '2rem',
-        position: 'relative',
-        width: '.4rem',
-
-    },
-    sidebarButton:{
-        backgroundColor: 'transparent',
-        border: '0',
-        color: '#008BF8',
-        cursor: 'pointer',
-    },
-    sidebarAImg:{
-        position:'absolute',
-        left:'0',
-        width:'100%',
-        transition:'opacity 0.25s ease-in-out',
-    },
-    sidebarAHover:{
-        opacity:0
-    },
-    sidebarSignout:{
-        position:'absolute',
-        bottom:'1rem'
-    },
-    sidebarSignoutButton:{
-        outline:'none'
-    },
-    sidebarSignoutButtonFA:{
-        fontSize:'2rem'
-    }
-}
-
+  },
+  signOut: {
+    position: 'absolute',
+    bottom: '1rem',
+  },
+  button: {
+    backgroundColor: 'transparent',
+    border: '0',
+    color: '#008bf8',
+    cursor: 'pointer',
+    outline: 'none',
+  },
+  buttonIcon: {
+    fontSize: '2rem',
+  },
+})
 
 export default Sidebar
